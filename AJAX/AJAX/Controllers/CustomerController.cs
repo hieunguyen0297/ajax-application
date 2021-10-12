6183ad51c9f202236ff7db6be2c1a92c2649618d
Hieu Nguyen <73616304+hieunguyen0297@users.noreply.github.com>
2021-10-12 12:22:58 -0400

    ajax finish

---
+++ AJAX/AJAX/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using AJAX.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAX.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<CustomerModel> customers = new List<CustomerModel>();
+
+        //Make a constructor to intialize the data
+        public CustomerController()
+        {
+            customers.Add( new CustomerModel { Id = 1, Name = "John", Age = 10});
+            customers.Add(new CustomerModel { Id = 2, Name = "Sherry", Age = 12 });
+            customers.Add(new CustomerModel { Id = 3, Name = "Melvin", Age = 14 });
+            customers.Add(new CustomerModel { Id = 4, Name = "Lias", Age = 16 });
+            customers.Add(new CustomerModel { Id = 5, Name = "Dace", Age = 18 });
+            customers.Add(new CustomerModel { Id = 6, Name = "Kavon", Age = 20 });
+        }
+        public IActionResult Index()
+        {
+            return View(customers);
+        }
+
+
+        public IActionResult ShowOnePerson(int id)
+        {
+
+            CustomerModel customer = customers.FirstOrDefault(x => x.Id == id);
+            return PartialView(customer);
+        }
+    }
+}
--- AJAX/AJAX/wwwroot/js/site.js
+++ AJAX/AJAX/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+
+    //this is for when the radio button is selected
+    $(".customerRadio").change(() => {
+        console.log("selected");
+        doCustomerUpdate();
+    })
+
+    function doCustomerUpdate(){
+        $.ajax({
+            type: "POST",
+            url: "customer/showoneperson",
+            data: $("form").serialize(),
+            success: (data) => {
+                $("#customerInformationArea").html(data);
+                $("#customerInformationArea").css("display", "block");
+            }
+
+        });
+    }
+
+    //this is done after the form is submit
+    $("#selectCustomer").click((e) => {
+        e.preventDefault();
+
+        $.ajax({
+            type: "POST",
+            url: "customer/showoneperson",
+            data: $("form").serialize(),
+            success: (data) => {
+                $("#customerInformationArea").html(data);
+                $("#customerInformationArea").css("display", "block");
+            }
+
+        });
+    })
+
+
+})
